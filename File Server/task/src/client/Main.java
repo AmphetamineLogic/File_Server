20e42e85cfe8efd42dd67e9dde29d9c91c76c464
Rabblerouser <phoenix.public@gmail.com>
2020-10-21 16:55:48 +0300

    Stage 3 completed (but tests fail)

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -4,23 +4,73 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 34522;
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
-            System.out.println("Client started!");
-            String msg = "Give me everything you have!";
-            System.out.println("Sent: " + msg);
-            dataOutputStream.writeUTF(msg);
-            System.out.println("Received: " + dataInputStream.readUTF());
-        } catch (IOException e) {
-            e.printStackTrace();
+        String[] input = new String[3];
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                 DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                 DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
+                System.out.print("Enter action (1 - get the file, 2 - create a file, 3 - delete the file): ");
+                input[0] = scanner.nextLine();
+                String response;
+
+                switch (input[0]) {
+                    case "1":
+                        System.out.print("Enter filename: ");
+                        input[1] = scanner.nextLine();
+                        dataOutputStream.writeUTF("GET " + input[1]);
+                        System.out.println("The request was sent.");
+                        response = dataInputStream.readUTF();
+                        if (response.startsWith("200")) {
+                            System.out.println("The content of the file is: " + response.substring(4));
+                        } else {
+                            System.out.println("The response says that the file was not found!");
+                        }
+                        break;
+                    case "2":
+                        System.out.print("Enter filename: ");
+                        input[1] = scanner.nextLine();
+                        System.out.print("Enter file content: ");
+                        input[2] = scanner.nextLine();
+                        dataOutputStream.writeUTF("PUT " + input[1] + " " + input[2]);
+                        System.out.println("The request was sent.");
+                        response = dataInputStream.readUTF();
+                        if ("200".equals(response)) {
+                            System.out.println("The response says that the file was created!");
+                        }
+                        else {
+                            System.out.println("The response says that creating the file was forbidden!");
+                        }
+                        break;
+                    case "3":
+                        System.out.print("Enter filename: ");
+                        input[1] = scanner.nextLine();
+                        dataOutputStream.writeUTF("DELETE " + input[1]);
+                        System.out.println("The request was sent.");
+                        response = dataInputStream.readUTF();
+                        if ("200".equals(response)) {
+                            System.out.println("The response says that the file was successfully deleted!");
+                        }
+                        else {
+                            System.out.println("The response says that the file was not found!");
+                        }
+                        break;
+                    case "exit":
+                        dataOutputStream.writeUTF("exit");
+                        return;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,21 +1,21 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
     private static final int PORT = 34522;
 
     public static void main(String[] args) {
-        System.out.println("Server started!");
         try (
                 ServerSocket server = new ServerSocket(PORT)
         ) {
-            Session session = new Session(server.accept());
-            session.start();
+            while (true) {
+                Session session = new Session(server.accept());
+                session.start();
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -31,16 +31,56 @@ class Session extends Thread {
     }
 
     public void run() {
-        try (
-                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream()))
-        {
-            String msg = "All files were sent!";
-            System.out.println("Received: " + dataInputStream.readUTF());
-            System.out.println("Sent: " + msg);
-            dataOutputStream.writeUTF(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+            try (
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
+
+                String input = dataInputStream.readUTF();
+                String[] request = input.split(" ");
+
+                if ("exit".equals(input)) {
+                    System.exit(0);
+                } else {
+                    File file = new File(System.getProperty("user.dir") +
+                            File.separator + "src" + File.separator + "server" + File.separator + "data" + File.separator + request[1]);
+                    System.out.println("Request: " + input);
+                    System.out.println("File: " + file.getAbsolutePath());
+                    switch (request[0]) {
+                        case "GET":
+                            if (file.exists()) {
+                                Scanner scanner = new Scanner(file);
+                                dataOutputStream.writeUTF("200 " + scanner.nextLine());
+                                scanner.close();
+                            } else {
+                                dataOutputStream.writeUTF("404");
+                            }
+                            break;
+                        case "PUT":
+                            if (file.exists()) {
+                                dataOutputStream.writeUTF("403");
+                            } else {
+                                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                                fileOutputStream.write(request[2].getBytes());
+                                fileOutputStream.close();
+                                dataOutputStream.writeUTF("200");
+                            }
+                            break;
+                        case "DELETE":
+                            if (file.exists()) {
+                                file.delete();
+                                dataOutputStream.writeUTF("200");
+                            } else {
+                                dataOutputStream.writeUTF("404");
+                            }
+                            break;
+                        case "EXIT":
+                            return;
+                    }
+                }
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+
     }
 }
